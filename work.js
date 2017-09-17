10355cebb6123108234a1f48aa5024b7e99fe0c4
Dongjoon Lee <dongjoon.wow@gmail.com>
2017-09-16 23:04:35 -0400

    update

--- work.js
+++ work.js
@@ -54,60 +54,96 @@ function filter(data) {
     }
     let descriptions = data.description.tags
     //count landmarks
-    /*let landmark = ""
-    if(data.categories[0].detail.landmarks[0].name) {
+    let landmark = ""
+    if(typeof(data.categories[0].detail.landmarks[0]) !== 'undefined') {
         landmark = data.categories[0].detail.landmarks[0].name
         console.log(landmark)
-    }*/
+    }
     //arrays of descriptors 
-    const city = ["buildling", "subway", "car", "street", "traffic", "skyscraper", "river", "bridge", "light"]
+    //use dictionary hash
+    /*const dict = {"building": "city", "subway": "city", "car": "city", "street": "city", "traffic": "city", "skyscraper": "city", "river": "city", "bridge": "city", "light": "city", "tower": "city", 
+    "sand": "beach", "ocean": "beach", "water": "beach", "blue": "beach", "reef": "beach", "palm": "beach", "towel": "beach",
+    "rock": "nature", "ocean": "nature", "water": "nature", "animal": "nature", "field": "nature", "green": "nature", "flower": "nature", "tree": "nature", "river": "nature", "plant": "nature", "mountain": "nature",
+    "water": "forest", "tree": "forest", "green": "forest", "brown": "forest", "grass": "forest", "wood": "forest", "plant": "forest", "flower": "forest",
+    "farm": "rural", "animal": "rural", "grass": "rural", "field": "rural", "cow": "rural", "sheep": "rural", "hill": "rural", "plant": "rural", "pasture": "rural", "herd": "rural",
+    "old": "historical", "stone": "historical", "church": "historical", "brick": "historical", "statue": "historical", "castle": "historical", "park": "historical"}*/
+    const city = ["building", "subway", "car", "street", "traffic", "skyscraper", "river", "bridge", "light", "tower"]
     const beach = ["sand", "ocean", "water", "blue", "reef", "palm", "towel"]
-    const nature = ["rock", "ocean", "water", "animal", "field", "green", "flower", "tree", "river", "plant"]
-    const forest = ["mountain", "water", "tree", "green", "brown", "grass", "wood", "plant", "flower"]
+    const nature = ["rock", "ocean", "water", "animal", "field", "green", "flower", "tree", "river", "plant", "mountain"]
+    const forest = ["water", "tree", "green", "brown", "grass", "wood", "plant", "flower"]
     const rural = ["farm", "animal", "grass", "field", "cow", "sheep", "hill", "plant", "pasture", "herd"]
+    const historical = ["old", "church", "stone", "brick", "statue","castle", "park"]
     //iterate each tag
     for (var i=0; i< Math.min(descriptions.length, 10); i++) {
         // do something to give ratings to each category      
         if(descriptions[i] === "nature"){
             scores.nature += 5
+            continue
         }
         if(descriptions[i] === "city"){
             scores.city += 5
+            continue
         }
         if(descriptions[i] === "beach"){
             scores.beach += 5
+            continue
         }
         if(descriptions[i] === "rural" || descriptions[i] === "country" || descriptions[i] === "countryside"){
             scores.rural += 5
+            continue
         }
         if(descriptions[i] === "forest"){
             scores.forest += 5
+            continue
         }
+        /*
+        for(key in dict) {
+            if(descriptions[i] === key) {
+                scores.window[dict[key]] += 1
+                continue
+            }
+        }*/
+        
         for(x in beach){
             if (descriptions[i] === beach[x]) {
                 scores.beach += 1
+                continue 
             }
         }
         for(x in nature){
             if (descriptions[i] === nature[x]) {
                 scores.nature += 1
+                continue 
             }
         }
         for(x in city){
             if (descriptions[i] === city[x]) {
                 scores.city += 1
+                continue 
             }
         }
         for(x in rural){
             if (descriptions[i] === rural[x]) {
                 scores.rural += 1
+                continue 
             }
         }
         for(x in forest){
             if (descriptions[i] === forest[x]) {
                 scores.forest += 1
+                continue 
             }
         }
     }
     console.log(scores)
+
+    //access data.json
+    $.getJSON( "data.json", function( data ) {
+        console.log(data)
+      var items = [];
+      $.each( data, function( key, val ) {
+        items.push( {key: val} );
+        //do comparison here, put min 5
+    });
+  });
 }
